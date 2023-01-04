fbb0134c6ce421ef43f379beb84f4f9fe074c305
TheNurseProgramer <alexis7unam@gmail.com>
2023-01-03 23:52:26 -0600

    se agrego la funcion de copiar al portapapeles y se ocultan y desocultan componentes segun el boton

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
  function encriptar (texto){
-    var new_texto = "";
-    var aux = Array.from(texto)
-    for (let i = 0; i < aux.length; i++) {
-        //console.log(aux[i])
-        switch (aux [i]){
-            case "a":
-                aux [i]= "ai"
-                break;
-            case "e":
-                aux [i]= "enter"
-                break;
-            case "i":
-                aux [i]= "imes"
-                break;
-            case "o":
-                aux [i]= "ober"
-                break;
-            case "u":
-                aux [i]= "ufat"
-                break;
-            default:
-                break;
+    if (texto == ""){
+        alerta()
+    }else {
+        document.getElementById("sin_texto").style.display = "none"
+        var new_texto = "";
+        //? se cambia de string a array
+        var aux = Array.from(texto)
+        //? se recorre el arreglo hasta encontrar el caracter especicifico y se sustituye
+        for (let i = 0; i < aux.length; i++) {
+            switch (aux [i]){
+                case "a":
+                    aux [i]= "ai"
+                    break;
+                case "e":
+                    aux [i]= "enter"
+                    break;
+                case "i":
+                    aux [i]= "imes"
+                    break;
+                case "o":
+                    aux [i]= "ober"
+                    break;
+                case "u":
+                    aux [i]= "ufat"
+                    break;
+                default:
+                    break;
+            }
+            new_texto += aux[i]
         }
-        new_texto += aux[i]
+        document.getElementById("texto_mostrar").innerText = new_texto
+        document.getElementById('texto_encriptar').value = ""
+        document.getElementById("con_texto").style.visibility = "visible"
     }
-    document.getElementById("texto_mostrar").innerText = new_texto
-    document.getElementById('texto_encriptar').value = ""
+   
  }
+
 function desencriptar (texto){
+    if (texto == ""){
+        alerta()
+    }else {
     var aux = Array.from(texto)
     new_texto = "";
+    //? se recorre el arreglo y se condiciona a encontrar la clave del encriptador, si es asi se eliminan los caracteres que la conforman
     for (let i = 0; i < aux.length; i++) {
-        //console.log(aux[i])
         switch (aux [i]){
             case "a":
                 if (aux[i]+aux[i+1]== "ai"){
@@ -70,4 +82,12 @@ function desencriptar (texto){
         }
     alert ("El mensaje es: \n"+new_texto)
     document.getElementById('texto_encriptar').value = ""
+    }
 }
+function copiar_texto (texto){
+    navigator.clipboard.writeText(texto)
+}
+function alerta (){
+    alert ("Asegurese de haber ingresado el texto!")
+}
+console.log (document.getElementById("sin_texto"))
